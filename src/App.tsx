c1a7119ddeadae6aebd2eafbea405bd219c6ebf4
Nailton Oliveira <nailton.me91@gmail.com>
2020-09-03 21:52:49 -0400

    adding style-guide to code and formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Marvel Heroes</h1>
-  );
-}
+const App: React.FC = () => {
+  return <h1>Marvel Heroes</h1>;
+};
 
 export default App;
